ee5ebe27024f3c553cfada12744ac623903e4118
kristofferfh <kristofferfh.kodehode@gmail.com>
2023-03-16 19:23:40 +0100

    Implemented lazy loading for cnews content

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,21 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// Lazyloading cnewsc
+$(window).on("load",function() {
+  const hiddenElements = document.querySelectorAll('.hidden');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    })
+  }, {
+    threshold: 0.8,
+  })
+
+  hiddenElements.forEach((el) => observer.observe(el));
+});
